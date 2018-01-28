0772850a4bcad5d279a77f790d1cb28059303744
tobozo <tobozo@users.noreply.github.com>
2018-01-28 15:32:37 +0100

    Update user_interface.h

--- tools/sdk/include/user_interface.h
+++ tools/sdk/include/user_interface.h
@@ -447,4 +447,9 @@ bool wifi_wps_start(void);
 typedef void (*wps_st_cb_t)(int status);
 bool wifi_set_wps_cb(wps_st_cb_t cb);
 
+typedef void (*freedom_outside_cb_t)(uint8 status);
+int wifi_register_send_pkt_freedom_cb(freedom_outside_cb_t cb);
+void wifi_unregister_send_pkt_freedom_cb(void);
+int wifi_send_pkt_freedom(uint8 *buf, int len, bool sys_seq);
+
 #endif
